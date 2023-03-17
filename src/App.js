f0143d6ba3ba1815d1460194774ae34eec0b1b09
Felipe Oliveira <felipe.oliveira@walljobs.com.br>
2023-03-17 10:46:16 -0300

    First trip in the React JS

--- src/App.js
+++ src/App.js
@@ -1,23 +1,29 @@
-import logo from './logo.svg';
+import React, {useState} from 'react';
 import './App.css';
+import MyBody from './MyBody';
 
 function App() {
+
+  const [str, setStr] = useState("Olá mundo");
+  //useState(false);
+  //useState([])
+  //useState(2)
+  function handleClick(){
+    setStr('Olá Felipe')
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h2>{str}</h2>
+      <button onClick={() => handleClick()}>Clique aqui</button>
+
+      <MyBody conteudo = "Body1"></MyBody>
+      <MyBody conteudo = "Body2"></MyBody>
+      <MyBody conteudo = "Body3" conteudo2="Felipe"></MyBody>
+      <MyBody></MyBody>
+      <MyBody></MyBody>
+      <MyBody></MyBody>
+
     </div>
   );
 }
---
+++ src/MyBody.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+const MyBody = (props) => {
+  return(
+    <div>
+    <h2>Meu componente {props.conteudo}</h2>
+    <p>{props.conteudo2}</p>
+    </div>
+  )
+}
+
+export default MyBody;
